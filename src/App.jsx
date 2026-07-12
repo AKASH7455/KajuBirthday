b96920b8c20d037ac24eedeeadf2d119723cf4be
AKASH7455 <ubayveer5@gmail.com>
2026-07-13 00:58:11 +0530

    complete the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-﻿import HomePage from './pages/HomePage'
+﻿import { useState, useEffect } from 'react'
+import HomePage from './pages/HomePage'
+import CountdownPage from './pages/CountdownPage'
+import { isUnlocked, UNLOCK_DATE } from './utils/countdown'
 import './styles/variables.css'
 import './styles/global.css'
 import './styles/animations.css'
 import './styles/responsive.css'
 
 function App() {
-  return <HomePage />
+  const [unlocked, setUnlocked] = useState(false)
+
+  useEffect(() => {
+    const checkUnlock = () => {
+      const shouldUnlock = isUnlocked()
+
+      if (shouldUnlock && !unlocked) {
+        setUnlocked(true)
+      }
+    }
+
+    checkUnlock() // Initial check
+    const interval = setInterval(checkUnlock, 1000)
+
+    return () => clearInterval(interval)
+  }, [unlocked])
+
+  return unlocked ? <HomePage /> : <CountdownPage />
 }
 
 export default App
